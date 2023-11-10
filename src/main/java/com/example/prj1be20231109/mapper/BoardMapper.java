1dc240c739b87ac199510c02992508b8f7fa9b55
yasupeter <peterleeful@gmail.com>
2023-11-10 10:51:09 +0900

    Fri, 11.10
    게시글 삭제 기능

--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.example.prj1be20231109.mapper;
 
 import com.example.prj1be20231109.domain.Board;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -29,4 +30,10 @@ public interface BoardMapper {
         WHERE id = #{id}
         """)
     Board selectById(Integer id);
+
+    @Delete("""
+        DELETE FROM board
+        WHERE id = #{id}
+        """)
+    int deleteById(Integer id);
 }
