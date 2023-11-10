8b44bb2dd1f1b2b1e9505992cf78406e0a564841
yasupeter <peterleeful@gmail.com>
2023-11-10 10:52:09 +0900

    Fri, 11.10
    게시글 수정 기능

--- src/main/java/com/example/prj1be20231109/controller/BoardController.java
+++ src/main/java/com/example/prj1be20231109/controller/BoardController.java
@@ -46,6 +46,12 @@ public class BoardController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
+    @PutMapping("edit")
+    public void edit(@RequestBody Board board) {
+//        System.out.println("board = " + board);
+        service.update(board);
+    }
 }
 
 
