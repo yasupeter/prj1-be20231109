4f5281876d37890a0f64d2014719b3def5831ef9
yasupeter <peterleeful@gmail.com>
2023-11-14 12:28:25 +0900

    Tues, 11.14 게시글 볼 때 member의 nickName이 writer에 표시

--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -15,16 +15,23 @@ public interface BoardMapper {
     int insert(Board board);
 
     @Select("""
-        SELECT id, title, writer, inserted
-        FROM board
-        ORDER BY id DESC
+        SELECT b.id, 
+               b.title, 
+               m.nickName writer, 
+               b.inserted
+        FROM board b JOIN member m ON b.writer = m.id
+        ORDER BY b.id DESC
         """)
     List<Board> selectAll();
 
     @Select("""
-        SELECT id, title, content, writer, inserted
-        FROM board
-        WHERE id = #{id}
+        SELECT b.id,
+               b.title, 
+               b.content, 
+               m.nickName writer, 
+               b.inserted
+        FROM board b JOIN member m ON b.writer = m.id
+        WHERE b.id = #{id}
         """)
     Board selectById(Integer id);
 
