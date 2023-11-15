93742314e70a2fbfe6e5f341e49aaa8e389559b9
yasupeter <peterleeful@gmail.com>
2023-11-15 12:21:57 +0900

    Wed, 11.15 댓글 쓰기 기능
    CREATE TABLE comment (
        id INT PRIMARY KEY AUTO_INCREMENT,
        boardId INT NOT NULL REFERENCES board(id),
        memberId VARCHAR(100) NOT NULL REFERENCES member(id),
        comment VARCHAR(500) NOT NULL ,
        inserted DATETIME NOT NULL DEFAULT NOW()
    );

---
+++ src/main/java/com/example/prj1be20231109/mapper/CommentMapper.java
@@ -0,0 +1,26 @@
+package com.example.prj1be20231109.mapper;
+
+import com.example.prj1be20231109.domain.Comment;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+
+    @Insert("""
+        INSERT INTO comment (boardId, comment, memberId) 
+        VALUES (#{boardId}, #{comment}, #{memberId})
+        """)
+    int insert(Comment comment);
+
+    @Select("""
+        SELECT *
+        FROM comment
+        WHERE boardId = #{boardId}
+        """)
+    List<Comment> selectByBoardId(Integer boardId);
+}
