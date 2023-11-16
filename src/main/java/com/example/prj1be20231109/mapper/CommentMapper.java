6b584be0b5073213658e0cb3dd1af84a7f400bd7
yasupeter <peterleeful@gmail.com>
2023-11-16 12:25:09 +0900

    Thurs, 11.16 댓글 작성자 별명 출력

--- src/main/java/com/example/prj1be20231109/mapper/CommentMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/CommentMapper.java
@@ -16,9 +16,16 @@ public interface CommentMapper {
     int insert(Comment comment);
 
     @Select("""
-        SELECT *
-        FROM comment
+        SELECT
+            c.id,
+            c.comment,
+            c.inserted,
+            c.boardId,
+            c.memberId,
+            m.nickName memberNickName
+        FROM comment c JOIN member m ON c.memberId = m.id
         WHERE boardId = #{boardId}
+        ORDER BY c.id DESC
         """)
     List<Comment> selectByBoardId(Integer boardId);
 
