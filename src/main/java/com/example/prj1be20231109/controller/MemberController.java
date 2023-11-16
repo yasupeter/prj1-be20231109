9137c9e6b424e573459dc4e8c1cf9106e509eb01
yasupeter <peterleeful@gmail.com>
2023-11-16 14:59:21 +0900

    Thurs, 11.16 탈퇴 후 로그아웃 처리

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -80,6 +80,7 @@ public class MemberController {
 
     @DeleteMapping
     public ResponseEntity delete(String id,
+                                 HttpSession session,
                                  @SessionAttribute(value = "login", required = false) Member login) {
         if (login == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 401
@@ -90,6 +91,8 @@ public class MemberController {
         }
 
         if (service.deleteMember(id)) {
+            session.invalidate();
+
             return ResponseEntity.ok().build();
         }
 
