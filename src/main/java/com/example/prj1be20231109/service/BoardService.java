b22b73b81245a33eea78969a403f9394a2e8cf31
yasupeter <peterleeful@gmail.com>
2023-11-17 16:26:50 +0900

    Fri, 11.17 이전페이지,다음페이지 번호 기능

--- src/main/java/com/example/prj1be20231109/service/BoardService.java
+++ src/main/java/com/example/prj1be20231109/service/BoardService.java
@@ -51,9 +51,17 @@ public class BoardService {
         int startPageNumber = (page - 1) / 10 * 10 + 1;
         int endPageNumber = startPageNumber + 9;
         endPageNumber = Math.min(endPageNumber, lastPageNumber);
+        int prevPageNumber = startPageNumber - 10;
+        int nextPageNumber = endPageNumber + 1;
 
         pageInfo.put("startPageNumber", startPageNumber);
         pageInfo.put("endPageNumber", endPageNumber);
+        if (prevPageNumber > 0) {
+            pageInfo.put("prevPageNumber", prevPageNumber);
+        }
+        if (nextPageNumber <= lastPageNumber) {
+            pageInfo.put("nextPageNumber", nextPageNumber);
+        }
 
         int from = (page - 1) * 10;
         map.put("boardList", mapper.selectAll(from));
